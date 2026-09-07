6647e5169a169b2076bbb7924ef95c55c324bdf8
karolinekjoslie-pixel <karolinekjoslie@gmail.com>
2026-09-07 11:22:51 +0200

    Ferdigstillt oppgave 0 og 1 med besvarelser i koden.

--- src/main/java/no/oslomet/cs/algdat/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1.java
@@ -1,19 +1,72 @@
 package no.oslomet.cs.algdat;
 
+import java.util.NoSuchElementException;
+
 public class Oblig1 {
 
     private Oblig1() {}
 
     // Oppgave 0
     public static int gruppeMedlemmer() {
-        throw new UnsupportedOperationException();
+        return 1;
+
         // Skal returne hvor mange som er i gruppa deres.
     }
 
     // Oppgave 1
-    public static int maks(int[] a) {throw new UnsupportedOperationException();}
+    public static int maks(int[] a) {
 
-    public static int ombyttinger(int[] a) {throw new UnsupportedOperationException();}
+        if(a.length == 0){
+            throw new NoSuchElementException("Tabellen er tom!");
+        }
+
+        for(int i = 1; i < a.length;i++){
+                if(a[i-1]>a[i]) {
+                    int temp = a[i-1];
+                    a[i-1] = a[i];
+                    a[i] = temp;
+
+            }
+        }
+            int storstVerdi = a.length - 1;
+            return a[storstVerdi];
+
+
+
+    }
+
+    //a) n - 1 sammenligninger
+    //b) Det blir færrest ombyttninger dersom listen allerede er sortert (best case)
+    //c) Worst case er n-1 ombyttninger, dersom tabeller er sortert synkende.
+    //d) Gjennomsnittlig ombyttninger vil befinne seg mellom beste og verste tilfellet, i beste tilfellet vil vi ha 0 inversjoner
+    // dersom listen allerede er sortert stigende, og i verste tilfellet vil vi ha n inversjoner.
+    // Gjennomsnittet befinner seg mellom disse to ytterpunktene,
+    // da ved en tilfeldig permutasjon er det 50 % sannsynlighet for ombyttninger,
+    // vi kan legge sammen best og worst case og dele på 2,
+    // det gir formelen for gjennomsnittet:
+    // n - 1 / 2 sammenligninger.
+    // For både worst og best case må vi likevel kjøre n-1 sammenligninger selv om vi ikke bytter så begge blir da 0(n),
+    // det vil også gjennomsnittlig case ha, da denne algoritmen går alltid gjennom hele tabellen
+    // en gang uavhengig av hvordan tallene er organisert
+    // Tidligere har vi sett på algoritmer der worst case er O(n^2),
+    // det vil si at forskjellen i kjøretid blir mye større dersom n er stor.
+    // Vår løkke går i alle tilfeller bare gjennom n elementer
+    // dermed er denne algoritmen generelt mer effektiv enn de forutgående
+
+
+    public static int ombyttinger(int[] a) {
+        int teller = 0;
+
+        for(int i = 1; i < a.length;i++){
+            if(a[i-1]>a[i]) {
+                int temp = a[i-1];
+                a[i-1] = a[i];
+                a[i] = temp;
+                teller++;
+            }
+        } return teller;
+    }
+       // throw new UnsupportedOperationException();}
 
     // Oppgave 2
     public static int antallUlikeSortert(int[] a) {throw new UnsupportedOperationException();}

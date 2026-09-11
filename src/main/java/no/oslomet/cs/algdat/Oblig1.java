82ace44c9a8283c88d5de3eeeba07229321950be
karolinekjoslie-pixel <karolinekjoslie@gmail.com>
2026-09-11 17:25:02 +0200

    Ferdigstillt oppgave 7 og 8.

--- src/main/java/no/oslomet/cs/algdat/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1.java
@@ -10,8 +10,6 @@ public class Oblig1 {
     // Oppgave 0
     public static int gruppeMedlemmer() {
         return 1;
-
-        // Skal returne hvor mange som er i gruppa deres.
     }
 
     // Oppgave 1
@@ -67,7 +65,7 @@ public class Oblig1 {
             }
         } return teller;
     }
-       // throw new UnsupportedOperationException();}
+
 
     // Oppgave 2
     public static int antallUlikeSortert(int[] a) {
@@ -91,23 +89,16 @@ public class Oblig1 {
     }
 
     // Oppgave 3
-    //Lag metoden public static int antallUlikeUsortert(int[] a). Tabellen
-    //a kan nå være en hvilken som helst heltallstabell, den trenger ikke være sortert, og
-    //kan ha flere like verdier.
-    //Metoden skal returnere hvor mange forskjellige verdier som er i a, og skal ikke
-    //endre på tabellens innhold.
-    //Metoden skal ikke bruke noen hjelpetabeller. Alt arbeid skal kun foregå innenfor
-    //tabellen a. Du kan derfor eksempelvis ikke lage en sortert kopi av a, eller lage en
-    //tabell over tidligere registrerte verdier. Du kan selvfølgelig lage hjelpevariabler
+
     public static int antallUlikeUsortert(int[] a) {
         int teller = 0;
 
         for (int i = 0; i < a.length; i++) {
-            boolean finnesFor = false;           //boolean variabel som holder styr på om et tall finnes fra før eller ikke, satt til false
+            boolean finnesFor = false;
 
             for (int j = 0; j < i; j++) {
                 if(a[i] == a[j]){
-                    finnesFor = true;            // hvis to tall er like så finnes tallet fra før
+                    finnesFor = true;
                     break;
                 }
             }
@@ -119,23 +110,6 @@ public class Oblig1 {
     }
 
     // Oppgave 4
-    //Lag metoden public static void sorter(int[] a, int fra, int til),
-    //som sorterer de elementene i en tabell som ligger på posisjoner mellom
-    //fra og til, inklusivt fra men eksklusivt til. Det vil si at intervallet [fra, til⟩ er
-    //intervallet som skal sorteres. Her er et eksempel:
-    //int[] a = {6, 10, 9, 4, 1, 3, 8, 5, 2, 7};
-    //sorter(a, 3, 8);
-    // nå skal vi ha a = {6, 10, 9, 1, 3, 4, 5, 8, 2, 7}
-    //Metoden skal ikke bruke hjelpetabeller og må være relativt effektiv. En av testene
-    //vil måle effektivitet på store tabeller.
-    //Metoden skal kaste en IndexOutOfBoundsException dersom fra er negativ
-    //eller dersom til er større enn tabellens lengde. Den skal ikke kaste feilmelding
-    //dersom fra er mindre enn til, men skal da ikke gjøre noe.
-
-    //Jeg vet nå at det ikke er boblesortering, insettningssortering da de i verste fall kan ha en tidkompleksitet på O(n^2)
-    // Jeg forsøker å lage en quicksort
-
-
     public static void sorter(int[] a, int fra, int til) {
         if (fra < 0 || til > a.length) {
             throw new IndexOutOfBoundsException();
@@ -176,18 +150,18 @@ public class Oblig1 {
 
 
     private static int parter0(int[] a, int v, int h, int skilleverdi) {
-        while (true)       {                            // Løkken fortsetter helt til vi selv stopper med return
-                                                        // Vi har to "pekere", v og h som blir [fra, til⟩ i vår oppgave
+        while (true)       {
 
-            while (v <= h && a[v] <= skilleverdi)       //Løkken kjører så lenge v ikke har gått forbi h(h er stoppeverdi for v), og
-                                                        //verdien på plass v/a[v] er mindre enn skilleverdien, flytt v ett hakk mot høyre.
+
+            while (v <= h && a[v] <= skilleverdi)
+
                 v++;
-            while (v <= h && a[h] >= skilleverdi)       // Løkken kjører også så lenge v<=h, v er stoppeverdi for h (så vice versa)
-                h--;                                    // og så lenge a[h]/verdien på plass h er større enn eller lik skillevrdien/pivoten
+            while (v <= h && a[h] >= skilleverdi)
+                h--;
 
             if (v < h)
-                bytt(a,v++,h--);                        // bytter om a[v] og a[h]
-            else  return v;                             //Når vi har gjort ferdig partisjoneringen, avsluttes metoden
+                bytt(a,v++,h--);
+            else  return v;
         }
     }
 
@@ -199,23 +173,9 @@ public class Oblig1 {
     }
 
 
-    //Bruk metoden sorter til å lage metoden public static void delsortering(int[] a).
-    //Den skal dele tabellen a i to sorterte deler. Venstre del skal inneholde alle oddetallene
-    //sortert, og høyre del alle partallene sortert. Følgende eksempel viser bruksområdet:
-    //int[] a = {6, 10, 9, 4, 1, 3, 8, 5, 2, 7};
-    //delsortering(a);
-
-    // Nå skal vi ha a = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10}.
-    //Tabellen a kan være tom, kan inneholde både negative og positive tall, kan
-    //bestå av kun oddetall, eller kun av kun partall. Ingen av disse mulighetene skal gi
-    //feilmelding
-
-    // Oppgave 5
-
-
     public static boolean erParTall(int tall) {
         boolean erPar = false;
-        if (tall % 2 == 0) {        //   return tall % 2 == 0;
+        if (tall % 2 == 0) {
             erPar = true;
         }
         return erPar;
@@ -251,22 +211,6 @@ public class Oblig1 {
         sorter(a, v, a.length);
     }
 
-
-    // Oppgave 6
-    //Lag metoden public static void rotasjon(char[] a). Metoden skal «rote-
-    //re» elementene i en tabell. En rotasjon gjøres ved at det siste elementet settes
-    //først, og alle andre element forskyves ett steg mot høyre. Følgende eksempel viser
-    //et bruksområde:
-    //char[] a = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    //rotasjon(a);
-
-    // a er nå {'J', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'}
-    //En rotasjon i en tom tabell eller tabell med nøyaktig ett element skal ikke gi
-    //feilmelding, men rotasjonen vil da ikke gjøre noe.
-
-    // Vi lagrer siste verdi i en temp før vi skyver tallene et steg mot høyre,
-    // deretter forksyver vi selve tallene mens løkka går baklengs
-    // til slutt legger vi verdien vi lagret på første plassen.
     public static void rotasjon(char[] a) {
 
         if (a.length <= 1) {
@@ -283,12 +227,55 @@ public class Oblig1 {
     }
 
     // Oppgave 7
-    public static void rotasjon(char[] a, int k) {throw new UnsupportedOperationException();}
+    public static void rotasjon(char[] a, int k) {
+        throw new UnsupportedOperationException();
+
+    }
 
     // Oppgave 8
-    public static String flett(String s, String t) {throw new UnsupportedOperationException();}
+    public static String flett(String s, String t) {
+        String resultat = "";                               // Vi starter med en tom streng
 
-    public static String flett(String... s) {throw new UnsupportedOperationException();}
+        int lengde = Math.min(s.length(), t.length());  // Bruker Javas innebygde Math klasse med min metoden for å finne det minste/minimum lengde på begge strengene
+
+        for (int i = 0; i < lengde; i++) {
+            resultat += s.charAt(i);                    // Her henter vi tegnet på plass [i], vi legger inn første tall fra  s og deretter fra t
+            resultat += t.charAt(i);
+        }
+
+        if (s.length() > lengde) {                  // Legger til ev resterende tegn fra s
+            resultat += s.substring(lengde);
+        }
+
+        if (t.length() > lengde) {                  // Legger til ev resterende tegn fra t
+            resultat += t.substring(lengde);
+        }
+
+        return resultat;
+    }
+
+
+    public static String flett(String... s) {
+        String resultat = "";
+        int lengste = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].length() > lengste) {
+                lengste = s[i].length();
+            }
+        }
+
+        for (int j = 0; j < lengste; j++) {
+            for (int i = 0; i < s.length; i++) {
+                if (j < s[i].length()) {
+                    resultat += s[i].charAt(j);
+                }
+            }
+        }
+
+        return resultat;
+
+    }
 
     // Oppgave 9
     public static int[] indeksSortering(int[] a) {throw new UnsupportedOperationException();}
